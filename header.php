e3b700882f2bdb2eb0aa2300ac7a71826da949ea
BodrunnaherSumi12 <16303031@iubat.edu>
2020-08-14 00:34:28 +0600

    Design Added

---
+++ header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+
+<html lang="">
+
+<head>
+<title>WFRTDS</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+
+<div class="wrapper row1">
+  <header id="header" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <div id="logo" class="fl_left">
+      <h1><a href="index.html">Wastage Food Reduction Trough Donation system</a></h1>
+      <p>Food For All</p>
+    </div>
+    <div id="quickinfo" class="fl_right">
+      <ul class="nospace inline">
+        <li><strong>Hotline:</strong><br>
+          +00 (123) 456 7890</li>
+      </ul>
+    </div>
+    <!-- ################################################################################################ -->
+  </header>
+  <nav id="mainav" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <ul class="clear">
+      <li class="active"><a href="index.html">Home</a></li>
+      <li><a href="#">Posts</a></li>
+      <li><a class="drop" href="#">Donations</a>
+        <ul>
+          <li><a href="pages/gallery.html">Gallery</a></li>
+          <li><a href="pages/full-width.html">Full Width</a></li>
+          <li><a href="pages/sidebar-left.html">Sidebar Left</a></li>
+          <li><a href="pages/sidebar-right.html">Sidebar Right</a></li>
+          <li><a href="pages/basic-grid.html">Basic Grid</a></li>
+        </ul>
+      </li>
+      <li><a class="drop" href="#">Registration</a>
+        <ul>
+          <li><a href="#">Registration As Donner</a></li>
+          <li><a href="#">Registration As Receiver</a>
+          </li>
+        </ul>
+      </li>
+      <li><a href="#">Donners</a></li>
+      <li><a href="#">Receivers</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Log IN</a></li>
+    </ul>
+    <!-- ################################################################################################ -->
+  </nav>
+</div>
