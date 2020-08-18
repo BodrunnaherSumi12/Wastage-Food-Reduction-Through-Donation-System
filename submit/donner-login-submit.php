2a7aadc7f81418dbe3545588ce2ced0f243bf11f
BodrunnaherSumi12 <16303031@iubat.edu>
2020-08-18 11:20:56 +0600

    New donner Tamplate add

--- submit/donner-login-submit.php
+++ submit/donner-login-submit.php
@@ -18,7 +18,7 @@
             if ($run->num_rows > 0) {
                 $user = $run->fetch_assoc();
                 $_SESSION['username'] = $user['username'];
-                header('location:../donner/donner-index.php');
+                header('location:../donner/index.php');
             } else {
                 $errors['username'] = "Invalid email or password"; 
                 $_SESSION['errors'] = $errors;
